c4bb5744be3e004c57de9748bc531a049f9e99e0
Bhaskar Verma <bhaskarverma.hacked00@gmail.com>
2023-06-23 18:12:41 +0530

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { Product } from "./types/products";
+import ProductTile from "./components/ProductTile";
 
 function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  fetch("https://fakestoreapi.com/products")
+    .then((res) => res.json())
+    .then((json) => setProducts(json));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+      }}
+    >
+      {products.map((product) => {
+        return <ProductTile key={product.id} product={product} />;
+      })}
     </div>
   );
 }
---
+++ src/components/ProductTile.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { Product } from "../types/products";
+
+interface ProductTileProps {
+  product: Product;
+}
+
+function ProductTile({ product }: ProductTileProps) {
+  const DESCRIPTION_LENGTH = 200;
+
+  const description =
+    product.description.length > DESCRIPTION_LENGTH
+      ? product.description.substring(0, DESCRIPTION_LENGTH) + "..."
+      : product.description;
+
+  return (
+    <div
+      style={{
+        width: 350,
+        height: 350,
+        border: "1px solid black",
+        margin: 10,
+        display: "flex",
+        flex: "0 0 auto",
+        flexDirection: "column",
+        gap: 10,
+      }}
+    >
+      <div>
+        <img src={product.image} alt={product.title} height={50} width={50} />
+      </div>
+
+      <span>{product.title}</span>
+      <div>{description}</div>
+    </div>
+    // Image at the top
+    // Title
+    // Description
+    // Row => Rating at the left, Price at the right
+    // Button => Add to cart
+  );
+}
+
+export default ProductTile;
